2824ddd8a9ba7b79c1fe5e6826ebe5432b6fb823
PoYen, Chen <PoYen.Chen@amd.com>
2024-05-28 02:42:58 +0000

    Add original pipeline files

--- example/ck_tile/01_fmha/gen/fmha_fwd_api.cpp
+++ example/ck_tile/01_fmha/gen/fmha_fwd_api.cpp
@@ -9,12 +9,43 @@ float fmha_fwd(fmha_fwd_traits t, fmha_fwd_args a, const ck_tile::stream_config&
     float r = -1;
     if(t.data_type.compare("fp16") == 0)
     {
-        if(t.hdim_q <= 128 && t.hdim_v <= 128)
+        if(t.hdim_q <= 64 && t.hdim_v <= 64)
         {
             if((t.is_group_mode == false) && (t.is_v_rowmajor == true) &&
                (t.mask_type == mask_enum::no_mask) && (t.has_bias == false) &&
-               (t.has_lse == true) && (t.do_fp8_static_quant == false) && (a.seqlen_q % 128 == 0) &&
-               (a.seqlen_k % 128 == 0) && (a.hdim_q % 128 == 0) && (a.hdim_v % 128 == 0))
+               (t.has_lse == false) && (t.do_fp8_static_quant == false) &&
+               (a.seqlen_q % 128 == 0) && (a.seqlen_k % 64 == 0) && (a.hdim_q % 64 == 0) &&
+               (a.hdim_v % 64 == 0))
+            {
+                using trait_ = fmha_fwd_traits_<64,
+                                                ck_tile::fp16_t,
+                                                false,
+                                                128,
+                                                64,
+                                                32,
+                                                64,
+                                                32,
+                                                64,
+                                                true,
+                                                ck_tile::BlockFmhaPipelineEnum::QRKSVS,
+                                                ck_tile::SimplifiedGenericAttentionMask<false>,
+                                                false,
+                                                false,
+                                                false,
+                                                false,
+                                                false,
+                                                false,
+                                                false>;
+                return fmha_fwd_dispatch<trait_>(s, a);
+            }
+        }
+        else if(t.hdim_q <= 128 && t.hdim_v <= 128)
+        {
+            if((t.is_group_mode == false) && (t.is_v_rowmajor == true) &&
+               (t.mask_type == mask_enum::no_mask) && (t.has_bias == false) &&
+               (t.has_lse == false) && (t.do_fp8_static_quant == false) &&
+               (a.seqlen_q % 128 == 0) && (a.seqlen_k % 128 == 0) && (a.hdim_q % 128 == 0) &&
+               (a.hdim_v % 128 == 0))
             {
                 using trait_ = fmha_fwd_traits_<128,
                                                 ck_tile::fp16_t,
@@ -29,7 +60,37 @@ float fmha_fwd(fmha_fwd_traits t, fmha_fwd_args a, const ck_tile::stream_config&
                                                 ck_tile::BlockFmhaPipelineEnum::QRKSVS,
                                                 ck_tile::SimplifiedGenericAttentionMask<false>,
                                                 false,
+                                                false,
+                                                false,
+                                                false,
+                                                false,
+                                                false,
+                                                false>;
+                return fmha_fwd_dispatch<trait_>(s, a);
+            }
+        }
+        else if(t.hdim_q <= 256 && t.hdim_v <= 256)
+        {
+            if((t.is_group_mode == false) && (t.is_v_rowmajor == true) &&
+               (t.mask_type == mask_enum::no_mask) && (t.has_bias == false) &&
+               (t.has_lse == false) && (t.do_fp8_static_quant == false) &&
+               (a.seqlen_q % 128 == 0) && (a.seqlen_k % 128 == 0) && (a.hdim_q % 256 == 0) &&
+               (a.hdim_v % 256 == 0))
+            {
+                using trait_ = fmha_fwd_traits_<256,
+                                                ck_tile::fp16_t,
+                                                false,
+                                                128,
+                                                128,
+                                                32,
+                                                256,
+                                                32,
+                                                256,
                                                 true,
+                                                ck_tile::BlockFmhaPipelineEnum::QRKSVS,
+                                                ck_tile::SimplifiedGenericAttentionMask<false>,
+                                                false,
+                                                false,
                                                 false,
                                                 false,
                                                 false,
